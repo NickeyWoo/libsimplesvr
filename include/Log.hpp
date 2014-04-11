321e46015b71209abbc3fca6dae62c3bfe31f689
nickeywoo <thenickey@gmail.com>
2014-04-11 07:27:46 -0400

    add log

--- include/Log.hpp
+++ include/Log.hpp
@@ -8,6 +8,22 @@
 #ifndef __LOG_HPP__
 #define __LOG_HPP__
 
+#include <utility>
+#include <vector>
+#include <string>
+#include <exception>
+#include <boost/noncopyable.hpp>
+#include <boost/format.hpp>
+
 extern const char* safe_strerror(int error);
+
+template<typename BinT>
+class Log :
+	public boost::noncopyable
+{
+public:
+
+};
+
 
 #endif // define __LOG_HPP__
